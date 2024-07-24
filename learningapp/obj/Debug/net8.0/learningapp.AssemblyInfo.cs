e9cfcc2ff1b75cd1d54eed4f9f843eddc6a28555
samchen20230808 <sam1207me@gmail.com>
2024-07-24 13:22:38 +0800

    Hello

--- learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
+++ learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a38900539118ecefee238b883e6735a63fd8db4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
